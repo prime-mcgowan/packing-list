426c84e2b51a4e2f64d0c6795e7c3c5192398f17
Laura McGowan <prime.lmcg@gmail.com>
2023-07-24 10:31:54 -0500

    line-through packed item written and working

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default function App() {
   function handleToggleItem(id) {
     setItems((items) =>
       items.map((item) =>
-        items.id === id ? { ...item, packed: !item.packed } : item
+        item.id === id ? { ...item, packed: !item.packed } : item
       )
     );
   }
